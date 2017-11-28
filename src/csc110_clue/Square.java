eb579c6243ff7073349c28aeb96b6c49cc2de2d2
ETHANATER50 <ethanater50@gmail.com>
2017-11-28 14:18:07 -0700

    added board files

---
+++ src/csc110_clue/Square.java
@@ -0,0 +1,23 @@
+package csc110_clue;
+
+public enum Square {
+
+	W, // wall
+	d, // door
+	H, // hall
+	L, // lounge
+	S, // study
+	l, // library
+	B, // billiard room
+	C, // conservatory
+	b, // ballroom
+	K, // kitchen
+	D, // dining room
+	O, // open space
+	s, // ms. scarlet
+	m, // mustard
+	p, // plum
+	w, // white
+	g, // mr. green
+	P, // peacock
+}
