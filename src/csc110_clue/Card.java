f8e66591841d9d7d412647c012d5c387901080ab
Linkdude09 <belac.ybloc@gmail.com>
2017-11-30 13:44:02 -0700

    changes

--- src/csc110_clue/Card.java
+++ src/csc110_clue/Card.java
@@ -1,19 +1,21 @@
 package csc110_clue;
 
 public class Card {
-	
+
 	private String name;
-	
+
 	private CardTypes type;
-	
+
 	private boolean isSuspect;
-	
-	
-	
+
 	Card(String name, CardTypes type) {
 		this.name = name;
 		this.type = type;
 		this.isSuspect = true;
+	}
+
+	public String toString() {
+		return this.name + "is a " + this.type + " Card.";
 	}
 
 	public CardTypes getType() {
@@ -29,11 +31,11 @@ public class Card {
 	}
 
 	public void setSuspect(boolean isSuspect) {
-		if(!this.isSuspect) {
-			throw new IllegalArgumentException(this.name + " is already exonerated, you cant reopen the case on them!!!");
+		if (!this.isSuspect) {
+			throw new IllegalArgumentException(
+					this.name + " is already exonerated, you cant reopen the case on them!!!");
 		}
 		this.isSuspect = isSuspect;
 	}
-
 
 }
--- src/csc110_clue/Game.java
+++ src/csc110_clue/Game.java
@@ -3,17 +3,33 @@ package csc110_clue;
 import java.util.ArrayList;
 
 public class Game {
-	
+
 	ArrayList<Card> Deck = new ArrayList<>();
-	
-	public void Run(){
-		
-		
+
+	public void Run() {
+
 	}
 
 	public void setup() {
-		
-		
+		this.generateDeck();
+		this.generateFile();
+
+	}
+
+	private void generateDeck() {
+		Deck.add(new Card("Professor Plum", CardTypes.Suspect));
+		Deck.add(new Card("Mr. Green", CardTypes.Suspect));
+		Deck.add(new Card("Col. Mustard", CardTypes.Suspect));
+		Deck.add(new Card("Ms. Scarlet", CardTypes.Suspect));
+		Deck.add(new Card("Mrs. White", CardTypes.Suspect));
+		Deck.add(new Card("Mrs. Peacock", CardTypes.Suspect));
+		Deck.add(new Card("Rope", CardTypes.Weapon));
+		Deck.add(new Card("Leadpipe", CardTypes.Weapon));
+		Deck.add(new Card("Knife", CardTypes.Weapon));
+	}
+
+	private void generateFile() {
+
 	}
 
 }
